e4a51454ddb781356de847921966bdbe5fd2aa48
Ricardo Vazquez <iamricardovazquez@gmail.com>
2018-10-31 12:42:25 -0400

    SVG
    Beginning work on SVG animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,10 @@ const IndexPage = () => (
             <Image />
           </div> */}
           <Link to="/video"> Watch our video</Link>
+          <svg width="100%" height="172" viewBox="0 0 100% 172" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z" fill="white" />
+            <animate repeatCount="indefinite" fill="freeze" attributeName="d" dur="10s" values="YOUR-SVG-VALUES" />
+          </svg>
         </div>
       </div>
     </div>   
