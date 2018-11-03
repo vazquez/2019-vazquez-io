34e2748ad0b4b73860799fb09d0f2167e77b44c8
Ricardo Vazquez <iamricardovazquez@gmail.com>
2018-11-03 13:44:11 -0400

    Adding example Component

---
+++ src/components/ExampleComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Component = () => (
+    <div className="Component">
+        <h3>React for Designers</h3>
+        <p>12 sections</p>
+    </div>
+)
+
+export default Component
